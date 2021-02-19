6ef509a4157ca8edce80ff526d9df5d0f81a827f
Dongxuan Li (李东轩)-云数据中心集团 <lidongxuan01@inspur.com>
2021-02-19 14:16:58 +0800

    file

---
+++ 线练习/data.js
@@ -0,0 +1,25 @@
+const data = [{
+    type: "苹果",
+    price: 2.34,
+    month: "一月"
+}, {
+    type: "苹果",
+    price: 2.4,
+    month: "二月"
+},{
+    type: "苹果",
+    price: 2.6,
+    month: "三月"
+},{
+    type: "苹果",
+    price: 2.9,
+    month: "四月"
+},{
+    type: "苹果",
+    price: 5.34,
+    month: "五月"
+}]
+
+module.exports = {
+    data
+}
